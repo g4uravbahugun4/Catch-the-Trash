a89e52151cead60719d0e88027da3bcebeb0086b
g4urav <86001972+g4uravbahugun4@users.noreply.github.com>
2022-10-29 12:51:39 +0530

    Update server.js
    cors

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ const PORT = process.env.PORT || 3000;
 
 
 nextApp.prepare().then(() => {
+  app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
   app.use("/api/signup", require("./api/signup"));
   app.use("/api/auth", require("./api/auth"));
   app.use("/api/highscores", require("./api/highscores"));
